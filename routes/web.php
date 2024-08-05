2982db118f828eb276966976123fc4dde941acb2
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-08-05 15:09:48 +0330

    Upload Video Form Imported & VideoController Created With All Required Methods and Videos Routes Group Created

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,27 @@
 
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
+// Main directory route
+Route::get('/', [IndexController::class, 'index'])->name('index');
+
+// Videos operations routes
+Route::controller(VideoController::class)->group(function () {
+    Route::get('/videos/create', 'create')->name('video.create');
+});
+
+// Dashboard route
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Auth operations routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/', [IndexController::class, 'index'])->name('index');
 
 require __DIR__ . '/auth.php';
