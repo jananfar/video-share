39588f8450e7e9ee8910bc27822381fc2cfc3e82
Amirreza Jananfar <amirrezajananfar@gmail.com>
2024-08-06 23:13:00 +0330

    CategorySeeder Class Created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            VideoSeeder::class
+            VideoSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
